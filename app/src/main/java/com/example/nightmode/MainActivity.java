148e452906e5e5ee77c60dd52f6dd3a48ab918fd
Musa <musagak"gmail.com>
2023-05-10 18:55:03 +0200

    After closing the App from background. application turn back in default mode even dark mode selected before closing App. but when user click on setting Activity the app mode change to dark.

--- app/src/main/java/com/example/nightmode/MainActivity.java
+++ app/src/main/java/com/example/nightmode/MainActivity.java
@@ -1,23 +1,59 @@
 package com.example.nightmode;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    NavController navController;
+    int NightMode;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         BottomNavigationView navigationView = findViewById(R.id.navi_btm);
-        NavController navController = Navigation.findNavController(this, R.id.fragmentContainerView);
+        navController = Navigation.findNavController(this, R.id.fragmentContainerView);
         NavigationUI.setupWithNavController(navigationView, navController);
 
+
+
+        sharedPreferences = getSharedPreferences("SharedPrefs", MODE_PRIVATE);
+        NightMode = sharedPreferences.getInt("NightModeInt", 1);
+        AppCompatDelegate.setDefaultNightMode(NightMode);
+
     }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        NightMode = AppCompatDelegate.getDefaultNightMode();
+        sharedPreferences = getSharedPreferences("SharedPrefs", MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        editor.putInt("NightModeInt", NightMode);
+        editor.apply();
+
+    }
+
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(LocaleHelper.onAttach(newBase));
+    }
+
+
 }
--- app/src/main/java/com/example/nightmode/Settings.java
+++ app/src/main/java/com/example/nightmode/Settings.java
@@ -2,33 +2,35 @@ package com.example.nightmode;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
-
 public class Settings extends Fragment {
 
     private static ImageButton changeMode;
-    Context context;
-    RadioGroup rg;
-
+    private Context mContext;
+    RadioGroup rg, langRG;
+    private RadioGroup languageRadioGroup;
+    Button langBtn;
+    Button changLangbtn;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-
-        }
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mContext = context;
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,9 +38,8 @@ public class Settings extends Fragment {
         View v = inflater.inflate(R.layout.fragment_settings, container, false);
 
         changeMode = v.findViewById(R.id.dark_mode);
-
-
-        switch (DarkModeHelper.getInstance().getPref(getActivity().getBaseContext())){
+        langBtn = v.findViewById(R.id.lang_btn);
+        switch (DarkModeHelper.getInstance().getPref(getActivity().getBaseContext())) {
             case "dark":
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
                 break;
@@ -46,66 +47,126 @@ public class Settings extends Fragment {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                 break;
             default:
-                if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.O){
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
-                }else{
+                } else {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
                 }
-
         }
 
-        changeMode.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final Dialog dialog = new Dialog(getContext());
-                dialog.setContentView(R.layout.custom_layout);
-                dialog.setTitle("Chose the mode");
-                rg = dialog.findViewById(R.id.radioGroupTheme);
-                switch (DarkModeHelper.getInstance().getPref(getActivity().getBaseContext())){
-                    case "dark":
-                        rg.check(R.id.radioButtonDark);
-                        break;
-                    case "light":
-                        rg.check(R.id.radioButtonLight);
-                        break;
-                    default:
-                        rg.check(R.id.radioButtonDefault);
-                }
-                rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                changeMode.setOnClickListener(new View.OnClickListener() {
                     @Override
-                    public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                        switch (i) {
-                            case R.id.radioButtonDark:
-                                DarkModeHelper.getInstance().setPref("dark", getActivity().getBaseContext());
-                                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                                dialog.dismiss();
+                    public void onClick(View view) {
+                        final Dialog dialog = new Dialog(mContext);
+                        dialog.setContentView(R.layout.custom_layout);
+                        rg = dialog.findViewById(R.id.radioGroupTheme);
+                        switch (DarkModeHelper.getInstance().getPref(getActivity().getBaseContext())) {
+                            case "dark":
+                                rg.check(R.id.radioButtonDark);
                                 break;
-                            case R.id.radioButtonLight:
-                                DarkModeHelper.getInstance().setPref("light", getActivity().getBaseContext());
-                                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                                dialog.dismiss();
+                            case "light":
+                                rg.check(R.id.radioButtonLight);
                                 break;
                             default:
-                                DarkModeHelper.getInstance().setPref("default", getActivity().getBaseContext());
-
-                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
-                                } else {
-                                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
-                                }
-                                dialog.dismiss();
+                                rg.check(R.id.radioButtonDefault);
                         }
+                        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                            @Override
+                            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                                switch (i) {
+                                    case R.id.radioButtonDark:
+                                        DarkModeHelper.getInstance().setPref("dark", getActivity().getBaseContext());
+                                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                                        dialog.dismiss();
+                                        getActivity().recreate();
+                                        break;
+                                    case R.id.radioButtonLight:
+                                        DarkModeHelper.getInstance().setPref("light", getActivity().getBaseContext());
+                                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                                        dialog.dismiss();
+                                        getActivity().recreate();
+                                        break;
+                                    default:
+                                        DarkModeHelper.getInstance().setPref("default", getActivity().getBaseContext());
+
+                                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                                        } else {
+                                            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
+                                        }
+                                        dialog.dismiss();
+                                        getActivity().recreate();
+
+                                }
+
+                            }
+                        });
+
+                        dialog.show();
+
 
                     }
                 });
 
-            dialog.show();
+        changLangbtn = v.findViewById(R.id.lang_btn);
 
-
+        changLangbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeLanguage();
             }
         });
 
 
         return v;
+
+    }
+
+    private void changeLanguage(){
+
+        final Dialog langDialog = new Dialog(mContext);
+        langDialog.setContentView(R.layout.lang_custom_layout);
+        languageRadioGroup = langDialog.findViewById(R.id.radioGroupLang);
+        String currentLanguageCode = LocaleHelper.getLanguage(getActivity());
+        switch (currentLanguageCode) {
+
+            case "en":
+                languageRadioGroup.check(R.id.radioButtonEnglish);
+                break;
+            case "fa":
+                languageRadioGroup.check(R.id.radioButtonFarsi);
+                break;
+            case "de":
+                languageRadioGroup.check(R.id.radioButtonGerman);
+                break;
+        }
+        languageRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+                switch (i) {
+                    case R.id.radioButtonEnglish:
+                        LocaleHelper.setLocale(mContext,"en");
+                        langDialog.dismiss();
+                        getActivity().recreate();
+                        break;
+                    case R.id.radioButtonFarsi:
+                        LocaleHelper.setLocale(mContext,"fa");
+                        langDialog.dismiss();
+                        getActivity().recreate();
+                        break;
+                    case R.id.radioButtonGerman:
+                        LocaleHelper.setLocale(mContext,"de");
+                        langDialog.dismiss();
+                        getActivity().recreate();
+                        break;
+                }
+
+            }
+        });
+        langDialog.show();
+
+
+
     }
 }
